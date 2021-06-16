8d3a85c24866872e2d767701a72e9e0b222e47e2
Dilmer01 <jhonmendieta@upeu.edu.pe>
2021-06-16 13:32:28 -0500

    EXÁMEN PARCIAL

---
+++ database/connect.js
@@ -0,0 +1,13 @@
+const database = {
+    
+    user: 'xwmffqvimfnwoo',
+    host: 'ec2-52-45-179-101.compute-1.amazonaws.com',
+    password: '4332af55126d58fb211e92d13f9b2323b8536fd15eead899159f8285a9a42d55',
+    database: 'd9sev7a2g4tam3',
+    port: 5432
+    
+  }
+  
+  module.exports = {
+      database
+  }
